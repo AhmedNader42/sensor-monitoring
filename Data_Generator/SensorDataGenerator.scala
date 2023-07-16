682b22a9cf7db567b79eb741f7b263301b61a702
Ahmed Nader <ahmednader111@gmail.com>
2023-07-17 00:11:56 +0300

    - Added SensorDataGenerator that writes randomly generated data to a JSON file. - Added SensorDataReader to read streaming JSON file

--- Data_Generator/SensorDataGenerator.scala
+++ Data_Generator/SensorDataGenerator.scala
@@ -21,3 +21,30 @@ import org.apache.spark.streaming.Seconds
 
 val streamingContext = new StreamingContext(sc, Seconds(2))
 
+import org.apache.spark.streaming.dstream.ConstantInputDStream
+val rawDStream = new ConstantInputDStream(streamingContext, sensorData)
+
+case class SensorData(sensorId: Int, timestamp: Long, value: Double)
+
+import scala.util.Try
+val schemaStream = rawDStream.flatMap { record =>
+    val fields = record.split(",")
+    Try {
+        SensorData(fields(0).toInt, fields(1).toLong, fields(2).toDouble)
+    }.toOption
+}
+
+import org.apache.spark.sql.SaveMode.Append
+schemaStream.foreachRDD { rdd => 
+    val df = rdd.toDF()
+    val outputPath = "/tmp/iotstream"
+
+    // def ts: String = 360.toString
+    // Appending to Parquet files gets more expensive with time
+    // That is why we need to split every time period in a production environment
+    // df.write.mode(Append).format("parquet").save(s"${outputPath}-$ts.parquet")
+    
+    df.write.mode(Append).format("json").save(s"$outputPath.json")
+}
+
+streamingContext.start()
